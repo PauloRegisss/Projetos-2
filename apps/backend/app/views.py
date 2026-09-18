b5b68516c55ab1770600c7d96f280ed44dc31b60
Pedro Rihan <tonyrihan25@gmail.com>
2026-09-18 17:56:05 -0300

    add views.py

--- apps/backend/app/views.py
+++ apps/backend/app/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import ContactMessage
+from django.utils import timezone
 
-# Create your views here.
+
+def contato(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+
+        ContactMessage.objects.create(
+            name=name,
+            email=email,
+            subject=subject,
+            message=message,
+            date=timezone.now()
+        )
+
+        return redirect('contato')
+
+    return render(request, 'core/contato.html')
